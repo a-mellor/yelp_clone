3f434a360fb5ff90441b4134d749773d38f54a61
a-mellor <alexander.mellor@gmail.com>
2016-07-13 14:02:09 +0100

    updated views to erb format

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
 
   config.omniauth :facebook, "1781876265389823", ENV["FACEBOOK_APP_SECRET"],
-                  callback_url: "http://localhost:3000/users/auth/facebook/callback"
+                  callback_url: "http://localhost:3000/users/auth/facebook/callback", scope "email", display "popup"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
